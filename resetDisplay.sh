49aa72906b0c8730e13c4b9081fe8762fe83244c
g <g5@sv3host.local.gmoyano.com>
2016-04-20 11:23:38 -0700

    set a native resolution

--- resetDisplay.sh
+++ resetDisplay.sh
@@ -1,5 +1,7 @@
 #!/bin/bash 
 set -x
 xrandr
-xrandr --output DVI-I-1 --mode 1680x1050 --left-of VGA-1
+# xrandr --output DVI-I-1 --mode 1680x1050 --left-of VGA-1
+xrandr --output VGA-1 --mode 1680x1050
+xrandr --output DVI-I-1 --mode 1280x1024 --right-of VGA-1
 xrandr
