c2bde40b5ca88741b1620acdada81c650b80fd01
George Moyano <g@gmoyano.com>
2016-06-23 08:51:03 -0700

    add argument check, package query list

--- log-yum-install.sh
+++ log-yum-install.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-set -x
-(
-	yum_command=${1}; package=${2}; log_label=${3}; 
-	echo "yum ${yum_command} ${package}" | tee -a ~/install-log/${log_label}_yum.log; 
-	date >> ~/install-log/${log_label}_yum.log; 
-	yum ${yum_command} ${package} 2>&1 | tee -a ~/install-log/${log_label}_yum.log;
-	rpm -ql ${package} | tee -a ~/install-log/${log_label}_yum.log;
-)
+set -o xtrace
+if [ ${#} -eq 3 ]; then
+	yum_command=${1}
+	package=${2}
+	log_label=${3}
+	echo "****  yum ${yum_command} ${package}  ****" | tee -a ~/install-log/${log_label}_yum.log
+	date >> ~/install-log/${log_label}_yum.log
+	yum ${yum_command} ${package} 2>&1 | tee -a ~/install-log/${log_label}_yum.log
+	rpm -ql ${package} | tee -a ~/install-log/${log_label}_yum.log
+else
+	echo "missing 3 arguments: yum_command package log_label"
+fi
