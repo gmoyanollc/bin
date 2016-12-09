2949096a184f5d83f76cd15ec7b01d8dcaa90ed8
George Moyano <g@gmoyano.com>
2016-12-08 16:12:26 -0800

    test that log folder exist, create

--- yum-install-logger.sh
+++ yum-install-logger.sh
@@ -4,10 +4,14 @@ if [ ${#} -eq 3 ]; then
 	yum_command=${1}
 	package=${2}
 	log_label=${3}
-	echo "****  yum ${yum_command} ${package}  ****" | tee -a ~/install-log/${log_label}_yum.log
-	date >> ~/install-log/${log_label}_yum.log
-	yum ${yum_command} ${package} 2>&1 | tee -a ~/install-log/${log_label}_yum.log
-	rpm -ql ${package} | tee -a ~/install-log/${log_label}_yum.log
+  log_dir=~/yum-install-logger
+  if [ ! -d "$log_dir" ]; then
+    mkdir ${log_dir}
+  fi
+	echo "****  yum ${yum_command} ${package}  ****" | tee -a ${log_dir}/${log_label}_yum.log
+	date >> ${log_dir}/${log_label}_yum.log
+	yum ${yum_command} ${package} 2>&1 | tee -a ${log_dir}/${log_label}_yum.log
+	rpm -ql ${package} | tee -a ${log_dir}/${log_label}_yum.log
 else
 	echo "missing 3 arguments: yum_command package log_label"
 fi
