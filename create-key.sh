a87afdc8fc593f396272964baef8f9e4f3c998be
George Moyano <G@GMoyano.com>
2017-01-16 09:05:29 -0800

    add script to create a random key file encrypted by a passphrase that may be used for dual-factor security * add script to create encrypted image disk * enhance encrypted disk mount for dual-factor security

---
+++ create-key.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/bash
+
+# MIT License
+#
+# Copyright (c) 2017 George Moyano (https://onename.com/gmoyano)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# source: https://wiki.gentoo.org/wiki/Sakaki%27s_EFI_Install_Guide/Preparing_the_LUKS-LVM_Filesystem_and_Boot_USB_Key
+# "create a (pseudo) random keyfile"
+# "keyfile will be encrypted with GPG (using a typed-in passphrase)"
+# enable "dual-factor security - both the (encrypted) keyfile, 
+#   and your passphrase (to decrypt it) will be required to access..."
+
+green="\033[32m"
+black="\033[0m"
+
+find ../* -maxdepth 0 -type d
+read -p "$(echo -e ${green}"? new key dir path: "${black})" key_dir;
+if [ -d "${key_dir}" ];
+then
+  ls -1 ${key_dir}
+else
+  echo "Error: ${key_dir} could not be found!"
+  exit 1
+fi
+read -p "$(echo -e ${green}"? key name (exclude '.gpg' suffix): "${black})" key_name;
+set -x
+export GPG_TTY=$(tty) 
+key_file=${key_dir}/${key_name}.gpg
+# "the cryptsetup system can support keyfiles up to and including 8192KiB"
+# "in practice, due to a off-by-one bug, it supports only keyfiles strictly
+#   less than 8MiB. We therefore create a keyfile of length (1024 * 8192) - 1 = 8388607 bytes."
+dd if=/dev/urandom bs=8388607 count=1 | gpg --symmetric --cipher-algo AES256 --output ${key_file}
+set +x
+if [ -f "${key_file}" ];
+then
+  ls -1 "${key_file}"
+  #gpg -q -d "${key_file}"
+else
+  echo "Error: ${key_file} could not be found!"
+  exit 1
+fi
+
