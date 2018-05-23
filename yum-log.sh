9e3d771683d0d22a51f6bdaceb9ecf148ac13988
g <g@gmoyano.com>
2018-05-23 08:39:55 -0700

    yum-log sudo

--- yum-log.sh
+++ yum-log.sh
@@ -10,7 +10,7 @@ if [ ${#} -eq 3 ]; then
   fi
 	echo "****  yum ${yum_command} ${package}  ****" | tee -a ${log_dir}/${log_label}_yum.log
 	date >> ${log_dir}/${log_label}_yum.log
-	yum ${yum_command} "${package}" 2>&1 | tee -a ${log_dir}/${log_label}_yum.log
+	sudo yum ${yum_command} "${package}" 2>&1 | tee -a ${log_dir}/${log_label}_yum.log
 else
 	echo "missing 3 arguments: yum_command package log_label"
 fi
