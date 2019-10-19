3dfadd8c7680b5afc6aad9ce88bd3df7374cbcf5
g <g@vm-51.g>
2019-10-18 19:10:57 -0700

    convert date logging

--- convert-date-format.sh
+++ convert-date-format.sh
@@ -1,5 +1,7 @@
 #! /usr/bin/bash
+# convert-date-format.sh
 read -p "Press any key to convert match '../../20..' to '..-..-20..n' in ${1}"
+echo "[INFO] backup ${1}"
 cp -v ${1} ${1}.bak
-sed -i 's/^\(..\)\/\(..\)\/20\(..\)/\1-\2-20\3/p' ${1}
-
+sed -i 's/^\(..\)\/\(..\)\/20\(..\)/\1-\2-20\3/' ${1}
+echo "[INFO] done"
