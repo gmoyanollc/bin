cf0d97dd83fdc2a3d61b1fa0ded3c27094b98f99
g <g@vm-51.g>
2020-02-07 09:36:33 -0800

    unlock partition logging

--- unlock-partition.sh
+++ unlock-partition.sh
@@ -2,6 +2,8 @@
 # unlock-partition.sh
 lsblk
 device=$(zenity --entry= --entry-text="/dev/sdb1" --text="/dev/sdb1")
+echo "[INFO] unlocking partition ${device}..."
 sudo clevis luks unlock -d ${device}
-
+echo "[INFO] done."
+read
 
