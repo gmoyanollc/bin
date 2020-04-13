cc0d8a97d715f4d860f68d975ab41663c1353976
g <g@vm-51.g>
2020-04-13 14:36:38 -0700

    initial mount-disk-image * lsblk columns

---
+++ mount-disk-image.sh
@@ -0,0 +1,3 @@
+# /bin/bash
+gnome-disk-image-mounter --writable
+~/bin/unlock-partition.sh
--- unlock-partition.sh
+++ unlock-partition.sh
@@ -1,9 +1,9 @@
 # !/usr/bin/bash
 # unlock-partition.sh
-lsblk
+lsblk --output NAME,TRAN,TYPE,MOUNTPOINT,STATE
 device=$(zenity --entry= --entry-text="" --text="[sd[a-z][n]] [loop[n]]")
 echo "[INFO] unlocking partition /dev/${device}..."
-sudo clevis luks unlock -d ${device}
+sudo clevis luks unlock -d /dev/${device}
 echo "[INFO] done, return code: ${?}"
 read
 
