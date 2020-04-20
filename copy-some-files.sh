5a268c4b716b5593981e29b254489c10ea421abf
gmoyanollc <g@gmoyano.com>
2020-04-20 14:31:00 -0700

    copy some files read fix

--- copy-some-files.sh
+++ copy-some-files.sh
@@ -9,7 +9,8 @@ echo FILE_DAYS_FILTER: ${FILE_DAYS_FILTER}
 echo SOURCE_FOLDER: ${SOURCE_FOLDER}
 find ${SOURCE_FOLDER} -ctime ${FILE_DAYS_FILTER} -name "${FILE_NAME_FILTER}" -type f
 echo TARGET_FOLDER: ${TARGET_FOLDER}
-read "[INFO] press any key to continue"
+echo "[INFO] press any key to continue"
+read
 find ${SOURCE_FOLDER} -ctime ${FILE_DAYS_FILTER} -name "${FILE_NAME_FILTER}" -type f | cpio -pdm ${TARGET_FOLDER}
 echo [INFO] done.
 
