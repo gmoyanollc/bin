d87958cc61581160493ee5733271386338be42fb
gmoyanollc <g@gmoyano.com>
2020-04-20 13:49:44 -0700

    wav to mpeg execution folder change

--- wav2mp3-ffmpeg.sh
+++ wav2mp3-ffmpeg.sh
@@ -3,6 +3,9 @@ echo -x
 # ffmpeg -i track01.wav -acodec mp3 -ab 64k track01.mp3
 # https://video.stackexchange.com/questions/19860/batch-conversion-into-a-new-folder-with-ffmpeg
 sourceFolder=${1}/
+originFolder=$(pwd)
+cd ${sourceFolder}
 # for i in *.wav; do ffmpeg -i "$i" "${i%.*}.mp3"; done
+for i in *.wav; do ffmpeg -i "${i}" -acodec mp3 -ab 128k "${i%.*}.mp3"; done
+cd ${originFolder}
 
-for i in ${sourceFolder}*.wav; do ffmpeg -i "${i}" -acodec mp3 -ab 128k "${sourceFolder}${i%.*}.mp3"; done
