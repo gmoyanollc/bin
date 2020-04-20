78f0e1571f5967830d00d71ad1c6203403d11ec9
gmoyanollc <g@gmoyano.com>
2020-04-20 15:28:09 -0700

    copy some deep files target tree with rsync

--- copy-some-deep-files.sh
+++ copy-some-deep-files.sh
@@ -5,15 +5,14 @@ FILE_DAYS_FILTER=${2}
 SOURCE_FOLDER=${3}
 TARGET_FOLDER=${4}
 echo FILE_NAME_FILTER: ${FILE_NAME_FILTER}
-echo FILE_DAYS_FILTER: ${FILE_DAYS_FILTER}
+#echo FILE_DAYS_FILTER: ${FILE_DAYS_FILTER}
 echo SOURCE_FOLDER: ${SOURCE_FOLDER}
-find ${SOURCE_FOLDER} -ctime ${FILE_DAYS_FILTER} -name "${FILE_NAME_FILTER}" -type f
+#find ${SOURCE_FOLDER} -ctime ${FILE_DAYS_FILTER} -name "${FILE_NAME_FILTER}" -type f
 echo TARGET_FOLDER: ${TARGET_FOLDER}
+#find ${SOURCE_FOLDER} -ctime ${FILE_DAYS_FILTER} -name "${FILE_NAME_FILTER}" -type f | cpio -dpmv ${TARGET_FOLDER}
+rsync -avmn --include "${FILE_NAME_FILTER}" --exclude "*.*" ${SOURCE_FOLDER} ${TARGET_FOLDER}
 echo "[INFO] press any key to continue"
 read
-ORIGIN_FOLDER=$(pwd)
-cd ${SOURCE_FOLDER}
-find . -ctime ${FILE_DAYS_FILTER} -name "${FILE_NAME_FILTER}" -type f | cpio -dpmv ${TARGET_FOLDER}
-cd ${ORIGIN_FOLDER}
+rsync -avm --include "${FILE_NAME_FILTER}" --exclude "*.*" ${SOURCE_FOLDER} ${TARGET_FOLDER}
 echo [INFO] done.
 
