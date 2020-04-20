4696df6df0cca8beb22ad6a3f0dfc230314b868e
gmoyanollc <g@gmoyano.com>
2020-04-20 14:27:34 -0700

    copy some files find fix

--- copy-some-files.sh
+++ copy-some-files.sh
@@ -7,7 +7,7 @@ TARGET_FOLDER=${4}
 echo FILE_NAME_FILTER: ${FILE_NAME_FILTER}
 echo FILE_DAYS_FILTER: ${FILE_DAYS_FILTER}
 echo SOURCE_FOLDER: ${SOURCE_FOLDER}
-${SOURCE_FOLDER} -ctime ${FILE_DAYS_FILTER} -name "${FILE_NAME_FILTER}" -type f
+find ${SOURCE_FOLDER} -ctime ${FILE_DAYS_FILTER} -name "${FILE_NAME_FILTER}" -type f
 echo TARGET_FOLDER: ${TARGET_FOLDER}
 read "[INFO] press any key to continue"
 find ${SOURCE_FOLDER} -ctime ${FILE_DAYS_FILTER} -name "${FILE_NAME_FILTER}" -type f | cpio -pdm ${TARGET_FOLDER}
