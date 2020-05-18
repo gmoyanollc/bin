5c36f194c417c094dc02db03513aa90ea38a8a5a
gmoyanollc <g@gmoyano.com>
2020-05-18 13:30:35 -0700

    unlock partition loop device * syncthing desktop config

--- unlock-partition.sh
+++ unlock-partition.sh
@@ -2,8 +2,13 @@
 # unlock-partition.sh
 lsblk --output NAME,TRAN,TYPE,MOUNTPOINT,STATE
 device=$(zenity --entry= --entry-text="" --text="[sd[a-z][n]] [loop[n]]")
-echo "[INFO] unlocking partition /dev/${device}..."
+echo "[INFO] unlocking partition at block device /dev/${device}..."
 sudo clevis luks unlock -d /dev/${device}
-echo "[INFO] done, return code: ${?}"
+echo "[INFO] return code: ${?}"
+if [[ ${?} -gt 0 ]] && [[ $(echo ${device} | grep -i loop) ]]; then
+  "[INFO] releasing loop block device /dev/${device} ..."
+  sudo losetup -d /dev/${device}
+fi
+echo "[INFO] done."
 read
 
