a20ddd607258ca120431f57a092332896aec16da
gmoyanollc <g@gmoyano.com>
2023-01-27 18:03:16 -0800

    create clevis tang disk passphrase loop and command printing

--- create-clevis-tang-disk.sh
+++ create-clevis-tang-disk.sh
@@ -15,42 +15,53 @@ TANG_URL=http://${TANG_URL_IP_PORT}
 echo TANG_URL: ${TANG_URL}
 echo "Enter to start or ctrl-c to quit"
 read
-set -x
-fallocate -l ${IMAGE_SIZE} ${IMAGE_FILE}
+(set -x; fallocate -l ${IMAGE_SIZE} ${IMAGE_FILE}) 
 # fallocate -l 1G test.img
-cryptsetup -y luksFormat ${IMAGE_FILE}
-# sudo cryptsetup -y luksFormat test.img
-set +x
+if [ "${?}" = "0" ]; then
+  (set -x; ls -al ${IMAGE_FILE})
+else  
+  exit ${?} 
+esle
+fi
+TRY=true
+
+while [ "${TRY}" = "true" ]; do
+  (set -x; cryptsetup -y luksFormat ${IMAGE_FILE})
+  # sudo cryptsetup -y luksFormat test.img
+  RETURN_CODE="${?}"
+  if [ "${RETURN_CODE}" = "0" ]; then
+    (set -x; sudo cryptsetup status ${IMAGE_FILE})
+    TRY=false
+  elif [ "${RETURN_CODE}" != "2" ]; then # not a passphrase problem
+    exit ${RETURN_CODE}
+  fi
+done
+
 DEVICE_LOOP=`losetup -f`
 echo DEVICE_LOOP: ${DEVICE_LOOP}
-set -x
-sudo losetup ${DEVICE_LOOP} ${IMAGE_FILE} 
-set +x
+(set -x; sudo losetup ${DEVICE_LOOP} ${IMAGE_FILE}) 
 # sudo losetup ${DEVICE_LOOP} test.img
 #LUKS_NAME=`date +%s | sha1sum | head -c 8`
 LUKS_NAME=${FILE_NAME}
 echo LUKS_NAME: ${LUKS_NAME}
-set -x
-sudo cryptsetup open ${DEVICE_LOOP} ${LUKS_NAME}
+(set -x; sudo cryptsetup open ${DEVICE_LOOP} ${LUKS_NAME})
 # sudo cryptsetup open ${DEVICE_LOOP} ${LUKS_NAME}
-sudo mkfs.ext4 /dev/mapper/${LUKS_NAME} -L ${IMAGE_FILE}
+(set -x; sudo mkfs.ext4 /dev/mapper/${LUKS_NAME} -L ${IMAGE_FILE})
 # sudo mkfs.ext4 /dev/mapper/${LUKS_NAME} -L test
-
-sudo clevis luks bind -d ${DEVICE_LOOP} tang '{"url":"'${TANG_URL}'"}'
+(set -x; sudo clevis luks bind -d ${DEVICE_LOOP} tang '{"url":"'${TANG_URL}'"}')
 # sudo clevis luks bind -d /dev/loop0 tang '{"url":"http://tang.server:9999"}'
-
 # manual mount with user permissions
-sudo mkdir ${MOUNT_PATH}/${USER}/${FILE_NAME}
+(set -x; sudo mkdir ${MOUNT_PATH}/${USER}/${FILE_NAME})
 # mkdir ~/test
-sudo mount /dev/mapper/${LUKS_NAME} "${MOUNT_PATH}/${USER}/${FILE_NAME}"
+(set -x; sudo mount /dev/mapper/${LUKS_NAME} "${MOUNT_PATH}/${USER}/${FILE_NAME}")
 # sudo mount /dev/mapper/${LUKS_NAME} ~/test
 # persist user permissions
-sudo chown -R ${USER}:${USER} "${MOUNT_PATH}/${USER}/${FILE_NAME}"
+(set -x; sudo chown -R ${USER}:${USER} "${MOUNT_PATH}/${USER}/${FILE_NAME}")
 # sudo chown -R userJoe:userJoe ~/test 
 
 # hereafter, run the following to mount disk using clevis
 # 
 # $ gnome-disk-image-mounter-rw.sh 
-# $ unlock-partition.sh
+# $ unt lock-partition.sh
 
 echo "done...disk created and mounted at ${MOUNT_PATH}/${USER}/${FILE_NAME}"
