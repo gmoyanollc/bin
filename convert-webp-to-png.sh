c7d5a4d63a1519f3487905d4e2cffdf0c453544b
gmoyanollc <g@gmoyano.com>
2026-06-18 20:27:02 -0700

    initial and modified version

---
+++ convert-webp-to-png.sh
@@ -0,0 +1,1 @@
+find *.webp | xargs -I % dwebp -v % -o %.png
--- format-disk-exfat.sh
+++ format-disk-exfat.sh
@@ -2,4 +2,4 @@ lsblk
 read -p "enter device, e.g., /dev/sdb1 : " device
 echo device=${device}
 umount -v ${device}
-sudo mkfs.vfat -F 32 ${device} -I
+sudo mkfs.exfat -V ${device}
