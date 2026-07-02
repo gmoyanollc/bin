6d7af9a2294488f7547851e33f432386d4f14c4b
gmoyanollc <g@gmoyano.com>
2026-07-01 17:10:17 -0700

    enclose in json object

--- generate-random-words.sh
+++ generate-random-words.sh
@@ -1,5 +1,8 @@
 wordCount="${1}"
 [ "${wordCount}" == "" ] && wordCount="5"
+echo -e "\n{"
 readarray -t randomWords < <(shuf -n ${wordCount} /usr/share/dict/words | xargs -d "\n" echo );
-echo -e "\n${randomWords[@]}" | tr ' ' '-'
-
+printf " \"words\": "
+echo -e "\"${randomWords[@]}\"," | tr ' ' '-'
+printf " \"generated\": \""; printf '%(%Y%m%d%H%M%S)T' -1; echo "\""
+echo -e "}\n"
